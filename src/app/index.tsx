f47295de784f0ef04b230578ce8fb121b469d917
김범수 <121074744+Devinix00@users.noreply.github.com>
2026-01-08 22:44:37 +0900

    axios instance 공통 예외 처리 toast 구현

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,12 @@
 import Router from "./router";
 import { QueryProvider } from "./providers";
+import { Toast } from "../shared/ui";
 
 export default function App() {
   return (
     <QueryProvider>
       <Router />
+      <Toast />
     </QueryProvider>
   );
 }
--- src/shared/api/instance.ts
+++ src/shared/api/instance.ts
@@ -3,6 +3,7 @@ import axios, {
   type AxiosInstance,
   type AxiosResponse,
 } from "axios";
+import toast from "react-hot-toast";
 
 const apiInstance: AxiosInstance = axios.create({
   baseURL: "",
@@ -14,6 +15,14 @@ apiInstance.interceptors.response.use(
   },
   (error: AxiosError) => {
     console.error(error);
+
+    const errorMessage =
+      (error.response?.data as { message?: string })?.message ||
+      error.message ||
+      "알 수 없는 오류가 발생했습니다.";
+
+    toast.error(errorMessage);
+
     return Promise.reject(error);
   }
 );
