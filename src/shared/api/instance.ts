be552e3de2cd42967e86812e25916c6f3d78dd27
김범수 <121074744+Devinix00@users.noreply.github.com>
2026-01-08 22:14:59 +0900

    axios setting

---
+++ src/shared/api/instance.ts
@@ -0,0 +1,21 @@
+import axios, {
+  AxiosError,
+  type AxiosInstance,
+  type AxiosResponse,
+} from "axios";
+
+const apiInstance: AxiosInstance = axios.create({
+  baseURL: "",
+});
+
+apiInstance.interceptors.response.use(
+  (response: AxiosResponse) => {
+    return response;
+  },
+  (error: AxiosError) => {
+    console.error(error);
+    return Promise.reject(error);
+  }
+);
+
+export default apiInstance;
